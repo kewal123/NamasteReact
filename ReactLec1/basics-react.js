11cb1c6bcb6bb2c524b58894006cd3d4799fa0a2
Keval Kulkarni <kevalkulkarni@Kevals-MacBook-Air.local>
2023-11-25 18:54:23 +0530

    Initial commit

---
+++ ReactLec1/basics-react.js
@@ -0,0 +1,6 @@
+const createElement =React.createElement("h1",{
+},"Namaste React")
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(createElement)
---
+++ ReactLec1/basics.js
@@ -0,0 +1,15 @@
+const divElement =document.createElement("div");
+
+const heading=document.createElement("h1");
+
+heading.innerText="Namaste Keval";
+
+
+
+divElement.appendChild(heading);
+
+const bodyElement=document.getElementsByTagName("BODY")[0];
+
+
+bodyElement.appendChild(divElement)
+
